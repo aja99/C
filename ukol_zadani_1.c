6b3b30f0f9000692b4c90c9f7bbdac7c20ce7779
Hung Ngo Quang <NQ.Hung.98@gmail.com>
2018-03-30 10:50:40 +0200

    Updated and modified the assignment of homework

--- ukol_zadani_1.c
+++ ukol_zadani_1.c
@@ -1,3 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define UNUSED(x) (void)(x)
+
 /*
  * Zadání:
  * Program přijme ze standardního vstupu soubor, který bude obsahovat
@@ -9,21 +16,25 @@
  * Řešení nahraj na Git, odevzdávej pouze soubor ukol_zadani_1.c
  *
  * FILE *soubor - bude obsahovat ukazatel na soubor, se kterým pracujeme
- * double array - bude obsahovat data, se kterými budeme pracovat
+ * int array - bude obsahovat data, se kterými budeme pracovat
  */
 
 // Načte data ze souboru a vloží je do pole s výsledky.
-double loadData(FILE *soubor, double array)  {
+int loadData(FILE *soubor, int array)  {
+  UNUSED(soubor);
+  UNUSED(array);
   // TO-DO
 }
 
 // Vypočítá průměr hodnot v poli a vrátí jeho hodnotu.
-double printAverage(double array) {
+int printAverage(int array) {
+  UNUSED(array);
   // TO-DO
 }
 
 // Vypočítá modus (nejčastěji zastoupená hodnota v poli) a vrátí jeho hodnotu.
-double printModus(double array) {
+int printModus(int array) {
+  UNUSED(array)
   // TO-DO
 }
 
@@ -35,7 +46,8 @@ double printModus(double array) {
  * 'i' - vypíše nápovědu o parametrech, které vyhodnocuje
  * '0' - ukončí funkce (return)
  */
-void menu(void) {
+void menu(int array) {
+  UNUSED(array)
   // TO-DO
 }
 
